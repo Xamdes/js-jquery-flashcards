ca7a9c194ebd88f5bff9e59bd197d7b63d7ec8a8
Hisato Kawaminami and Steven Colburn <steven@>
2018-05-30 09:07:01 -0700

    finish layout for jQuery and html flashcards.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,98 @@
 $(function()
 {
-  $(".clickable").click(function() {
+  $(".clickable-javascript").click(function() {
     $("#show-javascript").toggle();
     $("#hide-javascript").toggle();
   });
+
+  $(".clickable-operators").click(function() {
+    $("#show-operators").toggle();
+    $("#hide-operators").toggle();
+  });
+
+  $(".clickable-variables").click(function() {
+    $("#show-variables").toggle();
+    $("#hide-variables").toggle();
+  });
+
+  $(".clickable-variables-name").click(function() {
+    $("#show-variables-name").toggle();
+    $("#hide-variables-name").toggle();
+  });
+
+  $(".clickable-functions").click(function() {
+    $("#show-functions").toggle();
+    $("#hide-functions").toggle();
+  });
+
+  $(".clickable-methods").click(function() {
+    $("#show-methods").toggle();
+    $("#hide-methods").toggle();
+  });
+
+  $(".clickable-arguments").click(function() {
+    $("#show-arguments").toggle();
+    $("#hide-arguments").toggle();
+  });
+
+  $(".clickable-parameters").click(function() {
+    $("#show-parameters").toggle();
+    $("#hide-parameters").toggle();
+  });
+
+  $(".clickable-return").click(function() {
+    $("#show-return").toggle();
+    $("#hide-return").toggle();
+  });
+
+  $(".clickable-chaining-methods").click(function() {
+    $("#show-chaining-methods").toggle();
+    $("#hide-chaining-methods").toggle();
+  });
+
+  $(".clickable-strings").click(function() {
+    $("#show-strings").toggle();
+    $("#hide-strings").toggle();
+  });
+
+  $(".clickable-booleans").click(function() {
+    $("#show-booleans").toggle();
+    $("#hide-booleans").toggle();
+  });
+
+  $(".clickable-undefined").click(function() {
+    $("#show-undefined").toggle();
+    $("#hide-undefined").toggle();
+  });
+
+  $(".clickable-nan").click(function() {
+    $("#show-nan").toggle();
+    $("#hide-nan").toggle();
+  });
+
+  $(".clickable-escape").click(function() {
+    $("#show-escape").toggle();
+    $("#hide-escape").toggle();
+  });
+
+  $(".clickable-alert").click(function() {
+    $("#show-alert").toggle();
+    $("#hide-alert").toggle();
+  });
+
+  $(".clickable-comments").click(function() {
+    $("#show-comments").toggle();
+    $("#hide-comments").toggle();
+  });
+
+  $(".clickable-jquery").click(function() {
+    $("#show-jquery").toggle();
+    $("#hide-jquery").toggle();
+  });
+
+  $(".clickable-attributes").click(function() {
+    $("#show-attributes").toggle();
+    $("#hide-attributes").toggle();
+  });
+
 });
